91166970a7da49fa479c6e132e6389e4bb5b631f
alexonbstudio <alexonbstudio@gmail.com>
2020-07-20 05:16:11 +0200

    Create debian.sh

---
+++ FULL-AIO/debian.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+clear
+
+echo "
+#############################################
+#											#
+#	Copyright 2020 BY AlexonbStudio			#
+#											#
+#											#
+#############################################
+"
+
+
+if [ $USER != "root" ]; then
+
+fi
+
+echo "Sorry this script need sudo -i (ROOT user)"
